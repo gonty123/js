4024940d19fcc23e96a81564a522062b7804ca4e
gonty123 <moyuanhang@sina.com>
2019-12-08 02:20:14 +0800

    no message

--- Comfirm.js
+++ Comfirm.js
@@ -1,1 +1,16 @@
-12333333333  
+let confirm = (title) => {
+	class Content extends React.Component {
+	    componentDidMount(){
+		    this.props.success()
+		}
+	    render() {
+		    return <div>{this.props.title}</div>
+	    }
+	}
+    return new Promise(function(resolve, reject) {
+		ReactDOM.render(
+		    <Content title = {title} success={()=>{resolve(true);}}/>,
+		    document.getElementById('comfirm')
+		);
+	});
+}
